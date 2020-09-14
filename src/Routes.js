285b9c87555026e4bfe6094efcb229a218cfd4bc
Juhee Jang <juheejang.dev@gmail.com>
2020-09-14 20:24:43 +0900

    초기설정 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import ItemDetail from './pages/ItemDetail/ItemDetail';
-import ItemList from './pages/ItemList/ItemList';
-import Main from './pages/Main/Main';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
+import ItemDetail from './Pages/ItemDetail/ItemDetail';
+import ItemList from './Pages/ItemList/ItemList';
+import Main from './Pages/Main/Main';
+import SignIn from './Pages/SignIn/SignIn';
+import SignUp from './Pages/SignUp/SignUp';
 
 class Routes extends React.Component {
   render() {
