26f44dc1cbd68bfa47dd916745c00f91abd62243
choichoikule <eoniii91@gmail.com>
2020-09-18 22:00:22 +0900

    ADD: map함수 실행조건 간소화

--- src/Pages/ItemList/ListBox/ListBox.js
+++ src/Pages/ItemList/ListBox/ListBox.js
@@ -7,21 +7,17 @@ class ListBox extends Component {
     const { items } = this.props;
     return (
       <ul className="listBox">
-        {items ? (
-          items.map((iteminfo, index) => (
-            <Item
-              key={index}
-              itemName={iteminfo.itemName}
-              itemPrice={iteminfo.itemPrice}
-              itemImage={iteminfo.itemImage}
-              itemReview={iteminfo.itemReview}
-              itemGrade={iteminfo.itemGrade}
-              isLiked={iteminfo.isLiked}
-            />
-          ))
-        ) : (
-          <p className="noResult">검색 결과가 없습니다.</p>
-        )}
+        {items?.map((iteminfo, index) => (
+          <Item
+            key={index}
+            itemName={iteminfo.itemName}
+            itemPrice={iteminfo.itemPrice}
+            itemImage={iteminfo.itemImage}
+            itemReview={iteminfo.itemReview}
+            itemGrade={iteminfo.itemGrade}
+            isLiked={iteminfo.isLiked}
+          />
+        ))}
       </ul>
     );
   }
