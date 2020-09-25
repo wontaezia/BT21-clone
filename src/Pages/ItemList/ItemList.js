78c0cbedb7e22c469ae850e51d310f6aef078ba6
Wontae <wontae1206@gmail.com>
2020-09-25 12:51:55 +0900

    Modify: ItemList 불필요한 import 삭제

--- src/Pages/ItemList/ItemList.js
+++ src/Pages/ItemList/ItemList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Filter from './Filter/Filetr';
 import ListBox from './ListBox/ListBox';
 import Pagination from './Pagination/Pagination';
